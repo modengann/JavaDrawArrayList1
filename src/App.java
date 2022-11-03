604aea0c8cb01801c70f84dd0061b56338b23b0c
modengann <99417989+modengann@users.noreply.github.com>
2022-11-03 10:23:09 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,20 @@
+/** YouTube link: https://youtu.be/75GQ8RwZ7jU
+ *  Task 1: Add objects of your choosing (ovals, rectangles, Images) to an ArrayList when the mouse button is clicked.
+ *  Task 2: On space bar pressed, have all objects in your ArrayList change to a random color. 
+ *          Task 2 Challenge: can you have them all change to the same random color (i.e. all blue, then all red, then all ...)
+ */
+
+import javadraw.*;
+import java.util.*;
+
+public class App extends Window{
+
+    public static void main(String[] args) {
+        Window.open();
+    }
+
+    public void start(){
+        
+    }
+
+}
